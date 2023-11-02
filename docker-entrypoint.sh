0665dc8c470aad906274af6420b5bc1bcf4285fc
Sam <sam.muhammad@shinesolutions.com>
2023-11-02 12:49:47 +1100

    Dockerfile is updated with more tools are added, helm, kubectl, terraform etc

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,3 +5,11 @@ set -e
 # Welcome Message
 echo "Default entrypoint Terraform $@"
 terraform --version
+kubectl version --client
+helm version    
+tfsec --version
+yq --version
+docker-compose-templer --version
+
+# To proceed with command execution
+exec $@
